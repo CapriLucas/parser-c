97b6b69d811de9fc2f0f3e170eb9d50753d24100
dhuallpa <diegohuallpacontreras@hotmail.com>
2020-07-28 21:35:45 -0300

    Mas cosas por hacer

--- TP-3/funcionesTP3.h
+++ TP-3/funcionesTP3.h
@@ -2,3 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
+void imprimirOpciones(){
+     printf("Eleccion del reporte:\n\n");
+     printf("1)Listado de identificadores encontrados y su cantidad de veces");
+}
