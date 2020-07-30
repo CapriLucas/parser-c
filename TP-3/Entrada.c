1b4ac945b4e0e3490ac3707d9e1c7b0439ea7ed2
matiagustin19 <mattifernan19@gmail.com>
2020-07-30 02:16:55 -0300

    faltan cosas

---
+++ TP-3/Entrada.c
@@ -0,0 +1,17 @@
+//123123
+/*asd */?'2"wsd" ""sad\""
+
+"sakdhasjdh"
+
+while 
+
+do
+
+for
+
+for
+
+36
+032
+0x25125
+0xa65a
--- TP-3/funcionesTP3.h
+++ TP-3/funcionesTP3.h
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 
+//Encontre una funcion: strtol(const char *str, char **endptr, int base) , que lo que hace es convertir el valor hexa a decimal 
+//aunque no entiendo por que no me covierte la cadena hexa que encontro a decimal ¿??
+
 void imprimirOpciones(){
      printf("Eleccion del reporte:\n\n");
      printf("1)Listado de identificadores encontrados y su cantidad de veces");
