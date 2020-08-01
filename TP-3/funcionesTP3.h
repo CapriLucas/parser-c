2b8f5205fca5dbf954aab6635e6770620484fca0
matiagustin19 <mattifernan19@gmail.com>
2020-08-01 20:44:14 -0300

    #define, pasaje de octal y hexa a decimal, otros

--- TP-3/funcionesTP3.h
+++ TP-3/funcionesTP3.h
@@ -2,19 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
-//funcion que "propias de c" que podramos usar expliquen para que sirve
-
-//Encontre una funcion: strtol(const char *str, char **endptr, int base) , que lo que hace es convertir el valor hexa a decimal
-//aunque no entiendo por que no me covierte la cadena hexa que encontro a decimal ¿??
-// En si en la funcion "str" es la cadena principal
-// "endptr" seria la parte de caracteres luego del ultimo caracter numerico
-//lo que hace la funcion es retornar un long int que es la parte numerica de la cadena principal "str"
-// EJEMPLO  hexaEnDec= (int)strtol("0X2 asdk", luegoDePrtNum, 16);
-//Creas una variable hexaEnDec por asi decir y lo que retorna la funcion lo convertis en int con casteo (int)
-//,y una variable luegoDePrtNum que no te sirve para nada pero no podes dejarlo vacio y base 16
-// tambien podes buscar una funcion hecha en c que quizas haya en internet sino funciona con strtol
-
 struct Nodo1
 {
      int nro;
@@ -179,22 +166,83 @@ void mostrarLista3(struct Nodo3 *ident)
           aux = aux->sig;
      }
 }
+
+
+ int sumarElementosLista(struct Nodo1* cdec){
+ int suma=0;
+ while(cdec!=NULL){
+      suma=suma+cdec->nro;
+      cdec=cdec->sig;
+ }
+ return suma;
+}
+
+long long HexadecimalADecimal(char hexadecimal[40]){
+    long long numeroDecimal, pos;
+    int i = 0, val, len;
+
+    numeroDecimal = 0;
+    pos = 1;
+
+    len = strlen(hexadecimal);
+    len--;
+
+    for(i=0; hexadecimal[i]!='\0'; i++)
+    {
+ 
+        if(hexadecimal[i]>='0' && hexadecimal[i]<='9')
+        {
+            val = hexadecimal[i] - 48;
+        }
+        else if(hexadecimal[i]>='a' && hexadecimal[i]<='f')
+        {
+            val = hexadecimal[i] - 97 + 10;
+        }
+        else if(hexadecimal[i]>='A' && hexadecimal[i]<='F')
+        {
+            val = hexadecimal[i] - 65 + 10;
+        }
+
+        numeroDecimal += val * pow(16, len);
+        len--;
+    }
+    
+return numeroDecimal;
+}
+
+
+long long OctalADecimal(int numeroOctal){
+    int numeroDecimal = 0, i = 0;
+    while(numeroOctal != 0)
+    {
+        numeroDecimal += (numeroOctal%10) * pow(8,i);
+        ++i;
+        numeroOctal/=10;
+    }
+    i = 1;
+    return numeroDecimal;
+}
+
+
+
+
+
 void opciones()
 {
      printf("\nINGRESE LA OPCION DE LO QUE DESEA HACER:\n\n");
-     printf("1)  GENERAR LISTA DE IDENTIFICADORES\n");
-     printf("2)  GENERAR LISTA DE LITERALES CADENA\n");
-     printf("3)  GENERAR LISTA DE PALABRAS RESERVADAS\n");
-     printf("4)  GENERAR LISTA DE CONSTANTES DECIMALES\n");
-     printf("5)  GENERAR LISTA DE CONSTANTES OCTALES\n");
-     printf("6)  GENERAR LISTA DE CONSTANTES HEXADECIMALES\n");
-     printf("7)  GENERAR LISTA DE CONSTANTES REALES\n");
-     printf("8)  GENERAR LISTA DE CONSTANTES CARACTER\n");
-     printf("9)  GENERAR LISTA DE CARACTERES DE PUNTUACION\n");
-     printf("10) GENERAR LISTA DE CARACTERES DE OPERACION\n");
-     printf("11) GENERAR LISTA DE CARACTERES DE COMENTARIOS\n");
-     printf("12) GENERAR LISTA DE CARACTERES DE CARACTERES NO RECONOCIDOS\n");
-     printf("13) SALIR DEL REPORTE\n");
+     printf("1)  Generar lista de identificadores\n");
+     printf("2)  Generar lista de literales cadenas\n");
+     printf("3)  Generar lista de palabras reservadas\n");
+     printf("4)  Generar listado de constantes decimales\n");
+     printf("5)  Generar listado de constantes octales\n");
+     printf("6)  Generar listado de constantes hexadecimales\n");
+     printf("7)  Generar listado de constantes reales\n");
+     printf("8)  Generar listado de constantes caracter\n");
+     printf("9)  Generar listado de caracteres de puntuacion\n");
+     printf("10) Generar listado de caracteres de operacion\n");
+     printf("11) Generar listado de comentarios \n");
+     printf("12) Generar listado de caracteres no reconocidos\n");
+     printf("13) Salir del reporte\n");
 }
 
 void imprimirOpciones()
@@ -214,7 +262,7 @@ void imprimirOpciones()
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 1: GENERAR LISTA DE IDENTIFICADORES.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE IDENTIFICADORES:\n");
+               printf("Listado de identificadores encontrados y su cantidad de veces: \n");
                mostrarLista1(id);
                break;
 
@@ -222,7 +270,7 @@ void imprimirOpciones()
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 2: GENERAR LISTA DE LITERALES CADENA.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE LITERALES CADENA:\n");
+               printf("Listado de literales cadena encontrados y la longitud de los mismos:\n");
                mostrarLista1(lt);
                break;
 
@@ -230,7 +278,7 @@ void imprimirOpciones()
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 3: GENERAR LISTA DE PALABRAS RESERVADAS.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE PALABRAS RESERVADAS:\n");
+               printf("Listado de palabras reservadas encontradas en el orden en el que se encontraron:\n");
                mostrarLista2(pr);
                break;
 
@@ -238,47 +286,47 @@ void imprimirOpciones()
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 4: GENERAR LISTA DE CONSTANTES DECIMALES.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CONSTANTES DECIMALES:\n");
-               mostrarLista1(id);
+               printf("Listado de constantes decimales encontradas y el total acumulado:\n");
+               mostrarLista1(id); /* Modificar */
                break;
 
           case 5:
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 5: GENERAR LISTA DE CONSTANTES OCTALES.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CONSTANTES OCTALES:\n");
-               mostrarLista1(id);
+               printf("Listado de constantes octales encontradas y su valor en decimal:\n");
+               mostrarLista1(id); /*Modificar*/
                break;
 
           case 6:
                printf("-------------------------------------------------------------------------");
-               printf("\nUSTED INGRESO LA OPCION 6: GENERAR LISTA DE CONSTANTES REALES.\n");
+               printf("\nUSTED INGRESO LA OPCION 6: GENERAR LISTA DE CONSTANTES HEXADECIMALES.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CONSTANTES HEXADECIMAL:\n");
-               mostrarLista1(id);
+               printf("Listado de constantes hexadecimales encontradas y su valor en decimal:\n");
+               mostrarLista1(id); /*Modificar*/
                break;
 
           case 7:
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 7: GENERAR LISTA DE CONSTANTES REALES.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CONSTANTES REALES:\n");
-               mostrarLista1(id);
+               printf("Listado de constantes reales encontradas junto con el valor de su mantisa y parte entera:\n");
+               mostrarLista3(cre);
                break;
 
           case 8:
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 8: GENERAR LISTA DE CONSTANTES CARACTER.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CONSTANTES CARACTER:\n");
-               mostrarLista1(id);
+               printf("Listado de constantes caracter encontradas segun su orden de aparicion:\n");
+               mostrarLista1(id); /*Modificar*/
                break;
 
           case 9:
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 9: GENERAR LISTA DE CARACTERES DE PUNTUACION.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CARACTERES DE PUNTUACION:\n");
+               printf("Listado de caracteres de puntuacion encontrados y la cantidad de veces que aparecen:\n");
                mostrarLista1(cop);
                break;
 
@@ -286,7 +334,7 @@ void imprimirOpciones()
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 10: GENERAR LISTA DE CARACTERES DE OPERACION.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CARACTERES DE OPERACION:\n");
+               printf("Listado de caracteres de operacion encontrados y la cantidad de veces que aparecen::\n");
                mostrarLista1(cpu);
                break;
 
@@ -294,16 +342,16 @@ void imprimirOpciones()
                printf("-------------------------------------------------------------------------");
                printf("\nUSTED INGRESO LA OPCION 11: GENERAR LISTA DE COMENTARIOS.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE COMENTARIOS:\n");
-               mostrarLista1(id);
+               printf("Listado de comentarios encontrados clasificados en multilinea/simples:\n");
+               mostrarLista1(id); /*Modificar*/
                break;
 
           case 12:
                printf("-------------------------------------------------------------------------");
-               printf("\nUSTED INGRESO LA OPCION 12: GENERAR LISTA DE CARACTERES RECONOCIDOS.\n");
+               printf("\nUSTED INGRESO LA OPCION 12: GENERAR LISTA DE CARACTERES NO RECONOCIDOS.\n");
                printf("-------------------------------------------------------------------------\n\n");
-               printf("LISTA DE CARACTERES NO RECONOCIDOS:\n");
-               mostrarLista1(id);
+               printf("Listado de cadenas y caracteres no reconocidos con su numero de linea:\n");
+               mostrarLista1(id); /*Modificar*/
                break;
           }
 
@@ -312,6 +360,6 @@ void imprimirOpciones()
 
      } while (opcionIngresada != 13);
      printf("-------------------------------------------------------------------------");
-     printf("\n\t\t\tREPORTE FINALIZADO\n");
+     printf("\n\t\t\tREPORTE FINALIZADO\n\n");
      printf("-------------------------------------------------------------------------\n\n");
 }
