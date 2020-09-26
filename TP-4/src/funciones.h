4e8f09352c4d4431df5abbcdfb5e658a0222c3d6
LucasCapri <capriatalucas@gmail.com>
2020-09-26 10:49:33 -0300

    Agrego funcion completa para crear la sublista de parametros

--- TP-4/src/funciones.h
+++ TP-4/src/funciones.h
@@ -21,8 +21,9 @@ struct NodoDecl
 struct NodoDecl *listaVar = NULL;
 struct NodoFunc *listaFunc = NULL;
 
-// Funcion para retornar el puntero a la lista que se va a almacenar en "*listaVar"  (el parametro *punt es el mismo puntero a la lista)
-struct NodoDecl *agregarAListaVar(struct NodoDecl *punt, char *id, char *tipo)
+// Funcion para retornar el puntero a la lista que se va a almacenar en la lista de variables o la de funciones
+// el parametro *punt es el mismo puntero a la lista
+struct NodoDecl *agregarAListaDecl(struct NodoDecl *punt, char *id, char *tipo)
 {
   struct NodoDecl *nuevo = (struct NodoDecl *)malloc((strlen(tipo) + strlen(id) + 2) * sizeof(struct NodoDecl));
   nuevo->id = id;
@@ -47,13 +48,29 @@ struct NodoDecl *agregarAListaVar(struct NodoDecl *punt, char *id, char *tipo)
   nuevo->sig = aux;
   return punt;
 }
-// Todavia no la defino, primero habria que hacer la gramatica de las declaraciones y definiciones
-// Idea de la funcion: Agregar un nodo a una lista de todas las funciones declaradas en el input
-// y en parametros se abre una sublista con la cantidad de parametros que posea la funcion en el archivo c
-struct NodoDecl *agregarAListaFunc(struct NodoDecl *punt, char *id, char *tipo)
+// Funcion para agregar un parametro a un nodo de una Funcion, la lista de parametros es una sub-lista de la de funciones
+// Para cada parametro de una funcion registrada, se corre esta funcion agregando el param a la sublista
+// En caso de no encontrar el id de dicha funcion ... (COMPLETAR). Por ultimo retorna el puntero a la lista de funciones
+struct NodoDecl *agregarParamAFunc(struct NodoDecl *puntListFunc, char *idFunc, char *tipoParam, char *idParam)
 {
+  struct NodoDecl *aux = puntListFunc;
+  while (aux != NULL && aux->id != idFunc)
+  {
+    aux = aux->sig;
+  }
+  if (aux == NULL)
+  {
+    // EN ESTE CASO NO SE PUEDE AGREGAR NINGUN PARAMETRO YA QUE LA FUNCION BUSCADA NO EXISTE
+  }
+  else
+  {
+    struct NodoDecl *param = aux->parametros;
+    param = agregarAListaDecl(param, idParam, tipoParam);
+  }
+  return puntListFunc;
 }
-// Esta funcion verifica que no sea haya declarado dos veces ni una variable ni una funcion
+
+// Esta funcion verifica que no se re-declare ni una variable ni una funcion
 // Devuelve 1 si se encuentra ya declarada
 // devuelve 0 caso contrario
 int busquedaDecl(struct NodoDecl *punt, char *idABuscar)
@@ -67,4 +84,54 @@ int busquedaDecl(struct NodoDecl *punt, char *idABuscar)
     return 0;
   else
     return 1;
+}
+
+void opciones()
+{
+  printf("\nINGRESE LA OPCION DE LO QUE DESEA HACER:\n\n");
+  printf("1)  Generar listado de variables declaradas\n");
+  printf("2)  Generar listado de funciones declaradas\n");
+  printf("3)  Generar listado de cantidad de sentencias\n");
+  printf("4) Salir del reporte\n");
+}
+
+void imprimirOpcionesReporte()
+{
+
+  int opcionIngresada;
+  printf("-------------------------------------------------------------------------");
+  printf("\n\t\t\tOPCIONES DISPONIBLES:\n");
+  printf("-------------------------------------------------------------------------\n\n");
+  do
+  {
+    opciones();
+    scanf("%d", &opcionIngresada);
+    switch (opcionIngresada)
+    {
+    case 1:
+      printf("-------------------------------------------------------------------------");
+      printf("\nUSTED INGRESO LA OPCION 1: GENERAR LISTA DE VARIABLES DECLARADAS.\n");
+      printf("-------------------------------------------------------------------------\n\n");
+      break;
+
+    case 2:
+      printf("-------------------------------------------------------------------------");
+      printf("\nUSTED INGRESO LA OPCION 2: GENERAR LISTA DE FUNCIONES DECLARADAS.\n");
+      printf("-------------------------------------------------------------------------\n\n");
+
+      break;
+
+    case 3:
+      printf("-------------------------------------------------------------------------");
+      printf("\nUSTED INGRESO LA OPCION 3: GENERAR LISTA DE CANTIDAD DE SENTENCIAS.\n");
+      printf("-------------------------------------------------------------------------\n\n");
+      break;
+
+      system("pause");
+      system("cls");
+    }
+  } while (opcionIngresada != 4);
+  printf("-------------------------------------------------------------------------");
+  printf("\n\t\t\tREPORTE FINALIZADO\n\n");
+  printf("-------------------------------------------------------------------------\n\n");
 }
